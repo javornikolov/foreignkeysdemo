718efd53d87967db49b398701036938c7df1b600
Yavor Nikolov <nikolov.javor@gmail.com>
2014-04-28 00:06:17 +0300

    add child model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140427195621) do
+ActiveRecord::Schema.define(version: 20140427202420) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "children", force: true do |t|
+    t.string   "name"
+    t.integer  "parent_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "parents", force: true do |t|
     t.string   "name"
